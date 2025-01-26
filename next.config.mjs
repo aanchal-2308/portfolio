b484c87bb4507347ca806f7aed4be1da1a1a3c5d
Aanchal Goyal <aanchalg@geekyants.com>
2025-01-26 13:34:36 +0530

    removed eslint

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["www.google.com"], // Allow images from Google
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
